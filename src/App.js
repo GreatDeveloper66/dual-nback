b7cfb729b33291b41a3a911240a00716b2a0ee46
Adam <adam@Mac-2.local>
2020-01-27 11:42:57 -0600

    Add import statements for HomePage and GamePage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import './App.scss';
+import HomePage from './views/pages/HomePage/HomePage';
+import GamePage from './views/pages/GamePage/GamePage'
+
 
 class App extends React.Component {
   render(){
