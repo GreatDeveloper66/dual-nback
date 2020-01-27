43345de713948289a38877a4707d6da18adccf88
Adam <adam@Mac-2.local>
2020-01-27 11:54:57 -0600

    Add GamePage stateless component to GamePage.js

---
+++ src/views/pages/GamePage/GamePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './GamePage.scss'
+
+const GamePage = () => {
+  return (
+    <div className="GamePage">
+      <h1>Game Page</h1>
+      <Link to='/GamePage'>Game Page</Link>
+    </div>
+  );
+};
