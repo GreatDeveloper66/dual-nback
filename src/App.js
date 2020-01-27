cdb5d9564b2cffba2ec99d5734bb6b9c70f270a6
Adam <adam@Mac-2.local>
2020-01-27 11:03:07 -0600

    Change stateless functional component to React Component in App.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,17 @@ import {Switch, Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <main>
+          Main App
+
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
