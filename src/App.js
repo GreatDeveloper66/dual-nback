a344d4cc82c8cd4e17cebbb97d1fc17a245f438d
Adam <adam@Mac-2.local>
2020-01-27 14:36:11 -0600

    fix import results page link typo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import './App.scss';
 import HomePage from './views/pages/HomePage/HomePage';
 import GamePage from './views/pages/GamePage/GamePage'
+import ResultsPage from './views/pages/ResultsPage/ResultsPage'
 
 
 class App extends React.Component {
@@ -13,6 +14,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path = '/' component={HomePage} />
             <Route exact path = '/GamePage' component={GamePage} />
+            <Route exact path = '/ResultsPage' component={ResultsPage} />
           </Switch>
         </main>
       </div>
