6a409e98dbb7c377e409c28fc4f94c432ae9fc25
Adam <adam@Mac-2.local>
2020-01-27 11:51:18 -0600

    Add HomePage Component to HomePage.js

---
+++ src/views/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './HomePage.scss'
+
+const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <h1>Home Page</h1>
+      <Link to='/GamePage'>Game Page</Link>
+    </div>
+  );
+};
